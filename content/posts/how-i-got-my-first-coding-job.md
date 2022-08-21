4bf409998f6846430e84c58ee2ac7ead39e587ab
Jeff <jefnic23@gmail.com>
2022-08-21 00:16:42 -0400

    initial commit

---
+++ content/posts/how-i-got-my-first-coding-job.md
@@ -0,0 +1,4 @@
+Title: Yet another "outsider lands a coding job" success story
+Date: 2022-08-20
+---
+There are more than enough cases of outsiders getting into the tech industry for other outsiders to feel good about their chances of likewise breaking in. I saw it happen with a few musician friends/acquaintances, and read many blogs and reddit posts from people describing how they got coding jobs without a "real" coding background. 
---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+AUTHOR = 'Jeff Nicholas'
+SITENAME = 'A Mere Reed'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('GitHub', 'https://github.com/jefnic23'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/JeffFNicholas'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
